f4aa4cf4a1f7ad6f89bbc0c14dd55a05faad69b7
ShagizMan <danielgetaneh2011@gmail.com>
2022-09-22 21:24:24 +0300

    change the dir

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,7 +10,7 @@ file_line { 'aaaaa':
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-file { '/etc/nginx/html/index.html':
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
