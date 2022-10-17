90e33cf6dbde2ece22b59d6f5ae8985069fc9f13
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-17 20:11:01 +0300

    fix the pep8 error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+          len(completed), len(todos)))
+    [print("\t {}".format(com)) for com in completed]
