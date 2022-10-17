d4683d0f18260ee660cd67ef05e74ababa4d6513
ShagizMan <danielgetaneh2011@gmail.com>
2022-10-17 20:25:03 +0300

    add s to the user

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,7 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "user/{}".format(argv[1])).json()
+    user = requests.get(url + "users/{}".format(argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
